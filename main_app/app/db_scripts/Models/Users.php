5b8a09faad7b276bab19435ea0fbab075493d05e
DimitrisKas <dimitriskas.1997@gmail.com>
2020-12-27 20:16:50 +0200

    (App) Clean up User Model comments/code

--- main_app/app/db_scripts/Models/Users.php
+++ main_app/app/db_scripts/Models/Users.php
@@ -16,8 +16,6 @@ class User
     const CINEMAOWNER = "CINEMAOWNER";
     const USER = "USER";
 
-    const ID_PREFIX = "u";
-
     public function __construct($name, $surname, $username, $password, $email, $role, $confirmed)
     {
         $this->name = $name;
@@ -29,6 +27,13 @@ class User
         $this->confirmed = $confirmed;
     }
 
+    /** Wraper function for creating User objects through Document-like arrays.
+     *  For Users without ID.
+     * @see CreateExistingUserObj
+     * @see fromDocumentWithID
+     * @param $doc 'Document object that contains all User data
+     * @return User Object with user Data
+     */
     public static function fromDocument($doc): User
     {
         return new User(
@@ -37,6 +42,13 @@ class User
         );
     }
 
+    /** Wraper function for creating User objects through Document-like arrays.
+     *  For Users with ID.
+     * @see CreateExistingUserObj
+     * @see fromDocument
+     * @param $doc 'Document object that contains all User data
+     * @return User Object with user Data
+     */
     public static function fromDocumentWithID($doc): User
     {
         return self::CreateExistingUserObj(
@@ -45,7 +57,27 @@ class User
         );
     }
 
+    /** Create a User object for User that already exists in Database. (i.e. has an ID)
+     * @param $id
+     * @param $name
+     * @param $surname
+     * @param $username
+     * @param $password
+     * @param $email
+     * @param $role
+     * @param $confirmed
+     * @return User Object of user with given data
+     */
+    public static function CreateExistingUserObj($id, $name, $surname, $username, $password, $email, $role, $confirmed):User
+    {
+        $user = new User($name, $surname, $username, $password, $email, $role, $confirmed);
+        $user->id = $id;
+        return $user;
+    }
 
+    /** Add self to database
+     * @return bool Success boolean
+     */
     public function addToDB():bool
     {
         if (empty($this->username))
@@ -110,81 +142,27 @@ class User
             logger("Error: ". curl_error($ch) . " .. errcode: " . curl_errno($ch));
         }
 
-
-        // Debugging
-//        logger("Raw result: ". $result);
-//        $res = json_decode($result, true);
-//        $data = $res['response']['data'][0];
-//        logger("Data:" . var_export($data, true));
-
         curl_close($ch);
 
         return false;
     }
 
-    private function generateID()
+
+    /** Gets all Users from the DB-Service
+     * @return array(bool $success, User $user, string $errorMsg)
+     */
+    public static function GetAllUsers():array
     {
-        do {
-            $this->id = getRandomString(9, $this::ID_PREFIX);
-        } while($this->checkIfUniqueID() === false);
-    }
-
-    public function checkIfUniqueID():bool
-    {
-        $conn = OpenCon(true);
-
-        $sql_str = "SELECT ID FROM Users WHERE id=?";
-        $stmt = $conn->prepare($sql_str);
-        $stmt->bind_param("s",$id);
-        $id = $this->id;
-
-        if (!$stmt->execute())
-            logger("[USER_DB] Check UID failed " . $stmt->error);
-
-        if ($stmt->affected_rows === 1)
-            return false;
-        else
-            return true;
-    }
-
-    public function checkIfUniqueUsername():bool
-    {
-        $conn = OpenCon(true);
-
-        $sql_str = "SELECT ID FROM Users WHERE username=?";
-        $stmt = $conn->prepare($sql_str);
-        $stmt->bind_param("s",$this->username);
-
-        if (!$stmt->execute())
-            logger("[USER_DB] Check Username failed " . $stmt->error);
-
-        if ($stmt->affected_rows === 1)
-            return false;
-        else
-            return true;
-    }
-
-    // static functions
-    public static function CreateExistingUserObj($id, $name, $surname, $username, $password, $email, $role, $confirmed):User
-    {
-        $user = new User($name, $surname, $username, $password, $email, $role, $confirmed);
-        $user->id = $id;
-        return $user;
-    }
-
-    public static function DeleteUser(string $id):bool
-    {
-        logger("Trying to delete user with id: " . $id);
-
+        logger("Getting all users");
         $ch = curl_init();
-        $url = "http://db-service/users/" . $id;
+        $url = "http://db-service/users";
+
 
         curl_setopt($ch,CURLOPT_URL, $url);
-        curl_setopt($ch,CURLOPT_CUSTOMREQUEST, "DELETE");
         curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
 
         // Execute post
-        logger("Sending Request... at ". $url);
+        logger("Sending Request...");
         $result = curl_exec($ch);
 
         // Retrieve HTTP status code
@@ -196,23 +174,45 @@ class User
         $err = curl_error($ch);
         curl_close($ch);
 
+
         // Parse results
-        if ($http_code == 204)
-            return true;
+        if ($http_code == 200)
+        {
+            logger("Retrieved all users");
+            $result = json_decode($result, true);
 
+            $users = array();
+            $i =0;
+            foreach ($result as $user_doc)
+            {
+                $users[$i++] =  User::fromDocumentWithID($user_doc);
+            }
+            return array(true, $users, "");
+        }
         else if ($http_code >= 400)
-            logger("User could not be deleted.");
-
+        {
+            logger("Users could not be retrieved");
+            return array(false, array(), $result);
+        }
         else if ($errno == 6)
+        {
             logger("Could not connect to db-service.");
-
+            return array(false, array(), "Could not connect to Database Service");
+        }
         else if ($errno != 0 )
-            logger("An error occured with cURL. \n
-                Error: ". $err . " .. errcode: " . $errno);
+        {
+            logger("An error occured with cURL.");
+            logger("Error: ". $err . " .. errcode: " . $errno);
+            return array(false, array(), "Internal Error: " . $err);
+        }
 
-        return false;
+        return array(false, array(), "Undefined Error");
     }
 
+    /** Gets all Users from the DB-Service
+     * @param $data object Document object that should contain properly defined keys with edit data
+     * @return bool Success boolean
+     */
     public static function EditUser($data): bool
     {
         logger("Trying to edit user with id: " . $data['user_id']);
@@ -287,18 +287,23 @@ class User
 
     }
 
-    public static function GetAllUsers():array
+    /** Deletes User from Database
+     * @param $id User's id to be deleted
+     * @return bool Success boolean
+     */
+    public static function DeleteUser(string $id):bool
     {
-        logger("Getting all users");
-        $ch = curl_init();
-        $url = "http://db-service/users";
+        logger("Trying to delete user with id: " . $id);
 
+        $ch = curl_init();
+        $url = "http://db-service/users/" . $id;
 
         curl_setopt($ch,CURLOPT_URL, $url);
+        curl_setopt($ch,CURLOPT_CUSTOMREQUEST, "DELETE");
         curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
 
         // Execute post
-        logger("Sending Request...");
+        logger("Sending Request... at ". $url);
         $result = curl_exec($ch);
 
         // Retrieve HTTP status code
@@ -310,38 +315,23 @@ class User
         $err = curl_error($ch);
         curl_close($ch);
 
-
         // Parse results
-        if ($http_code == 200)
-        {
-            logger("Retrieved all users");
-            $result = json_decode($result, true);
+        if ($http_code == 204)
+            return true;
 
-            $users = array();
-            $i =0;
-            foreach ($result as $user_doc)
-            {
-                $users[$i++] =  User::fromDocumentWithID($user_doc);
-            }
-            return array(true, $users, "");
-        }
         else if ($http_code >= 400)
-        {
-            logger("Users could not be retrieved");
-            return array(false, array(), $result);
-        }
+            logger("User could not be deleted.");
+
         else if ($errno == 6)
-        {
             logger("Could not connect to db-service.");
-            return array(false, array(), "Could not connect to Database Service");
-        }
+
         else if ($errno != 0 )
-        {
-            logger("An error occured with cURL.");
-            logger("Error: ". $err . " .. errcode: " . $errno);
-            return array(false, array(), "Internal Error: " . $err);
-        }
+            logger("An error occured with cURL. \n
+                Error: ". $err . " .. errcode: " . $errno);
+
+        return false;
     }
+
 
     /** Tries to login a user based on given Username and Password.
      * On Success, returns User model.
@@ -392,19 +382,21 @@ class User
         else if ($http_code >= 400)
         {
             logger("User was not created.");
-            return array(false, null, $result);
+            return array(false, array(), $result);
         }
         else if ($errno == 6)
         {
             logger("Could not connect to db-service.");
-            return array(false, null, "Internal error");
+            return array(false, array(), "Internal error");
         }
         else if ($errno != 0 )
         {
             logger("An error occured with cURL.");
             logger("Error: ". $err . " .. errcode: " . $errno);
-            return array(false, null, "Internal error");
+            return array(false, array(), "Internal error");
         }
+
+        return array(false, array(), "Undefined error");
     }
 
 }
