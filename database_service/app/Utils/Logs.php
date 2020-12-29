819d345f6b5d66e884b1cb353a7d7b010cebfd9b
DimitrisKas <dimitriskas.1997@gmail.com>
2020-12-29 21:28:42 +0200

    (App+DB-Serv) Upgrade Logs logic to improve performance
    * If over 10K Characters, return as many as possible, (previous version would fail to bring logs if under 10K chars)
    * if over 100K Characters, trim down to 10K

--- database_service/app/Utils/Logs.php
+++ database_service/app/Utils/Logs.php
@@ -3,13 +3,31 @@
 // Save msg to log
 function logger($msg)
 {
+
+    $filename = $_SERVER['DOCUMENT_ROOT'].'/Utils/Logs/log.txt';
     $msg = date("[d/m] h:i:sa") . ": ". $msg . "\n";
-    file_put_contents( $_SERVER['DOCUMENT_ROOT'].'/Utils/Logs/log.txt', $msg, FILE_APPEND);
+
+    file_put_contents( $filename, $msg, FILE_APPEND);
 }
 
 // Get Last 10000 characters from logs
 function getLogs(): string
 {
-    return file_get_contents( $_SERVER['DOCUMENT_ROOT'].'/Utils/Logs/log.txt', false, null, -10000);
+    $filename = $_SERVER['DOCUMENT_ROOT'].'/Utils/Logs/log.txt';
+    $filesize = filesize($filename);
+
+    if ($filesize > 100000)
+    {
+        // Trim down to 10K bytes if over 100K Bytes
+        $data = file_get_contents( $filename, false, null, -10000);
+        file_put_contents($filename, $data);
+    }
+
+
+    // Get last 10K characters(bytes) or as many as possible
+    if ($filesize < 10000)
+        return file_get_contents( $filename, false, null, -$filesize);
+    else
+        return file_get_contents( $filename, false, null, -10000);
 
 }
